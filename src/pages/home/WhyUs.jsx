f2f02c3f3a37b29ca0770881a857d8491cb95f56
Eugene Kiprop Sang <eugenekipropsangtg@gmail.com>
2023-12-26 22:00:54 +0300

    Why us animation

--- src/pages/home/WhyUs.jsx
+++ src/pages/home/WhyUs.jsx
@@ -1,106 +1,96 @@
-import React, { useState, useEffect, cloneElement } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../../css/why-us.css';
 
-
 function placeSentence(text) {
-    function placeStringInArray(inputString) {
-        const arrayLength = 12;
-        const resultArray = Array(arrayLength).fill(' ');
+  function placeStringInArray(inputString) {
+    const arrayLength = 12;
+    const resultArray = Array(arrayLength).fill(' ');
 
-        const stringLength = inputString.length;
-        const startIndex = Math.floor((arrayLength - stringLength) / 2);
+    const stringLength = inputString.length;
+    const startIndex = Math.floor((arrayLength - stringLength) / 2);
 
-        for (let i = 0; i < stringLength; i++) {
-            resultArray[startIndex + i] = inputString[i];
-        }
-
-        return resultArray;
+    for (let i = 0; i < stringLength; i++) {
+      resultArray[startIndex + i] = inputString[i];
     }
 
-    /**
-     * 
-     * @param {String} sentence 
-     */
-    function placeStrings(sentence) {
-        const words = sentence.split(' ');
-        let rows = [];
-        var curString = '';
-        for (let word of words) {
-            if ((curString + word).length > 11) {
-                rows.push(curString);
-                curString = word;
-            } else {
-                const separator = curString.length ? '·' : ' ';
-                curString = curString + separator + word;
-            }
-        }
+    return resultArray;
+  }
 
+  function placeStrings(sentence) {
+    const words = sentence.split(' ');
+    let rows = [];
+    let curString = '';
+
+    for (let word of words) {
+      if ((curString + word).length > 11) {
         rows.push(curString);
-
-        return rows;
+        curString = word;
+      } else {
+        const separator = curString.length ? '·' : ' ';
+        curString = curString + separator + word;
+      }
     }
 
-    const ans = placeStrings(text).map(str => placeStringInArray(str));
-    
-    return ans;
-};
+    rows.push(curString);
+    return rows;
+  }
 
-
-
-const GridRow = ({sentence}) => {
-    const ids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-    
-    const classes = sentence ? sentence.map(d=>d != ' ' && d ?'filled': 'empty'): ids.map(d=>'empty');
-
-
-    return (
-        <div className="why-us-grid-row">
-            {ids.map((id) => {
-                return (
-                    <div className={`why-us-cell ${classes[id - 1]}`} key={id}>
-                        <span>{sentence? sentence[id-1] : ' '}</span>
-                    </div>
-                )
-            })}
-        </div>
-    )
+  return placeStrings(text).map((str) => placeStringInArray(str));
 }
 
-const WhyUs = () => {
-    const ids = [1, 2, 3];
-    var curIndex = 0;
 
-    const texts = [
-        "Sleek design wows users",
-        "got Strong online presence",
-        "Seamless on any device",
-        "Tech for digital leadership",
-        "Fortify against online threats",
-        "Your success, our priority",
-        "Bring vision to life"
-    ].map(d=>placeSentence(d));
-
-    const [sentence, setSentence] = useState(texts[curIndex]);
-
-    setInterval(()=>{
-        if(curIndex>= texts.length){
-            curIndex =0;
-        }
-
-        console.log(curIndex);
-        setSentence(texts[curIndex++]);
-    }, 2500)
+const GridRow = ({ sentence }) => {
+    const ids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+    const classes = sentence ? sentence.map((d) => (d !== ' ' && d ? 'filled' : 'empty')) : ids.map(() => 'empty');
+  
     return (
-        <div className="why-us">
-            <div className="why-us-container">
-                {ids.map((id) => {
-                    return (
-                        <GridRow key={id} sentence={sentence[id-1]} />
-                    )
-                })}
-            </div>
-        </div>
+      <div className="why-us-grid-row">
+        {ids.map((id) => (
+          <div className={`why-us-cell ${classes[id - 1]}`} key={id}>
+            <span>{sentence ? sentence[id - 1] : ' '}</span>
+          </div>
+        ))}
+      </div>
     );
+  };
+
+
+const WhyUs = () => {
+  const ids = [1, 2, 3];
+  const texts = [
+    "Sleek design wows users",
+    "Strong online presence",
+    "Seamless on any device",
+    "Tech for digital leadership",
+    "Fortify against online threats",
+    "Your success our priority",
+    "Bring vision to life",
+  ].map((d) => placeSentence(d));
+
+  const [sentenceIndex, setSentenceIndex] = useState(0);
+  const [sentence, setSentence] = useState(texts[sentenceIndex]);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setSentenceIndex((prevIndex) => (prevIndex + 1) % texts.length);
+    }, 2500);
+
+    return () => clearInterval(intervalId);
+  }, [texts]);
+
+  useEffect(() => {
+    setSentence(texts[sentenceIndex]);
+  }, [sentenceIndex]);
+
+  return (
+    <div className="why-us">
+      <div className="why-us-container">
+        {ids.map((id) => (
+          <GridRow key={id} sentence={sentence[id - 1]} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default WhyUs;

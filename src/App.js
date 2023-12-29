c63375a0c52871b5dcba9dbeddd5d42f15d9e8c8
Eugene Kiprop Sang <eugenekipropsangtg@gmail.com>
2023-12-30 00:08:36 +0300

    Yago page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import './App.css';
 import Background from './background.jsx';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './pages/home/index.js';
+import HomePage from './pages/HomePage.jsx';
 import Error404 from './pages/404/404.jsx';
 
 function App() {
   return (
     <Router>
       <div className="App">
-      <Background />
-        <Routes>
-          <Route path="/" element={<HomePage/>} />
-          <Route path="*" element={<Error404/>} />
-        </Routes>
+        <Background />
+        <div className='content'>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="*" element={<Error404 />} />
+          </Routes>
+        </div>
       </div>
     </Router>
   );

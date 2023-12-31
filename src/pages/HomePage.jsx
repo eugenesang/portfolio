2e110a4ae5673d9e720fef34c008f28a750fba3e
Eugene Kiprop Sang <eugenekipropsangtg@gmail.com>
2023-12-31 14:01:14 +0300

    Page loader addition

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,12 +7,20 @@ import Projects from '../components/Projects';
 import Contact from '../components/Contact';
 import Footer from '../components/Footer';
 
+
 import "../css/yago-home-page.css";
+import PageLoader from '../components/PageLoader';
 
 const HomePage = () => {
 
     const [menuOpen, setMenuOpen] = useState(false);
     const [menuState, setMenuState] = useState('deactive');
+    const [pageLoading, setPageLoading] = useState(true);
+
+    setTimeout(()=>{
+        setPageLoading(false);
+    }, 10000)
+
 
     function toggleMenu() {
         setMenuOpen(!menuOpen);
@@ -23,7 +31,7 @@ const HomePage = () => {
         }
     }
     return (<div className="home-page">
-
+        {pageLoading && <PageLoader/>}
         <Menu toggleMenu={toggleMenu} showMenu={menuState} />
         <Nav toggleMenu={toggleMenu} showMenu={menuState} />
 

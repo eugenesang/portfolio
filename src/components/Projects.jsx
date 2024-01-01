9dd049ebf0fb1cb75210226247584d0d5b7f1218
Eugene Kiprop Sang <eugenekipropsangtg@gmail.com>
2024-01-01 21:06:54 +0300

    Removed initial projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -71,49 +71,7 @@ const Projects = props => {
                     </p>
                 </div>
                 <div className="projects-wrapper">
-                    <Project
-                        title="Anonymous Message Board."
-                        img={'https://raw.githubusercontent.com/yagoestevez/fcc-portfolio/master/src/Components/Projects/Images/AnonMsgBoard.jpg'}
-                        tech="js css react node"
-                        link="https://yagoestevez-anon-msg-board.glitch.me/"
-                        repo="https://github.com/yagoestevez/anonymous-message-board"
-                    >
-                        <small>
-                            Built using Node, Express, MongoDB, CSS + Bulma, React.js and React Router.
-                        </small>
-                        <p>
-                            This is a full-stack website that I made which lets the users read and post anonymous
-                            messages and replies.
-                        </p>
-                    </Project>
-                    <Project
-                        title="Stock Price Checker."
-                        img={'https://raw.githubusercontent.com/yagoestevez/fcc-portfolio/master/src/Components/Projects/Images/StockPriceChecker.jpg'}
-                        tech="js node css"
-                        link="https://yagoestevez-stock-price-checker.glitch.me/"
-                        repo="https://github.com/yagoestevez/fcc-stock-price-checker"
-                    >
-                        <small>
-                            Built using Node, Express, MongoDB, JS + jQuery + Axios, CSS + Bootstrap and Pug.
-                        </small>
-                        <p>
-                            Another full-stack website to check the current value of any requested stock in the
-                            market.
-                        </p>
-                    </Project>
-                    <Project
-                        title="Personal Library."
-                        img={'https://raw.githubusercontent.com/yagoestevez/fcc-portfolio/master/src/Components/Projects/Images/PersonalLibrary.jpg'}
-                        tech="js node css"
-                        link="https://yagoestevez-personal-library.glitch.me/"
-                        repo="https://github.com/yagoestevez/fcc-personal-library"
-                    >
-                        <small>Built using Node, Express, MongoDB, JS + jQuery, CSS + Bootstrap and Pug.</small>
-                        <p>
-                            A full-stack website to store book titles and comments to each book into a remote
-                            database.
-                        </p>
-                    </Project>
+                   
                     <Project
                         title="Issue Tracker."
                         img={'https://raw.githubusercontent.com/yagoestevez/fcc-portfolio/master/src/Components/Projects/Images/IssueTracker.jpg'}

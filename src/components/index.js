7dc3fb4db8e7bfdaf5302de30a8273c698578c9e
Eugene Kiprop Sang <eugenekipropsangtg@gmail.com>
2024-04-11 16:36:31 +0300

    components export

---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+
+export { default as Layout } from "./Layout"
+export { default as NavBar } from "./Navbar/index.js"
+export { default as Footer } from "./Footer"
+export { default as Header } from "./Header"
+export { default as Container } from "./Container"
+export { default as Intro } from "./Intro"
+export { default as Projects } from "./Projects"
+export { default as Quote } from "./Quotes"
+export { default as Contact } from "./Contact"
+
+

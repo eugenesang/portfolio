aab21c9593a7d2728e8da8ee4178a9457a65cdbd
Eugene Kiprop Sang <eugenekipropsangtg@gmail.com>
2024-04-11 16:55:16 +0300

    home page

---
+++ src/pages/index.js
@@ -0,0 +1,26 @@
+import { Layout, Intro, Container, Projects, Contact, Footer, Quote } from "../components";
+import { useEffect } from "react";
+import Aos from "aos"
+import { DataContextProvider } from "../context/DataContext";
+
+export default function HomePage() {
+
+    useEffect(() => {
+        Aos.init({ duration: "1000" })
+    }, [])
+
+    return (
+        <DataContextProvider>
+            <Layout>
+                <Container>
+                    <Intro />
+                    <Projects />
+                </Container>
+                <Quote />
+                <Contact />
+                <Footer />
+            </Layout>
+        </DataContextProvider>
+    )
+}
+

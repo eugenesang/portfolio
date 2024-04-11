19447959c271afac9d38a18425bc70bd869d027a
Eugene Kiprop Sang <eugenekipropsangtg@gmail.com>
2024-04-11 16:33:05 +0300

    remove background and update styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import './App.css';
+import './css/global.css';
 import Background from './background.jsx';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './pages/HomePage.jsx';
+import HomePage from './pages/index.js';
 import Error404 from './pages/404/404.jsx';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Background />
         <div className='content'>
           <Routes>
             <Route path="/" element={<HomePage />} />

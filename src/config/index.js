7374c5cf9ff35749b3972d40b32421233d44a728
Eugene Kiprop Sang <eugenekipropsangtg@gmail.com>
2024-04-12 15:13:33 +0300

    use my emailjs codes

--- src/config/index.js
+++ src/config/index.js
@@ -2,8 +2,8 @@
 
 // EMAILJS Config Data
 
-export const EMAILJS_TEMPLATE_ID = "template_kh9buql"
+export const EMAILJS_TEMPLATE_ID = "template_jx0i7zz"
 
-export const EMAILJS_SERVICE_ID = "service_hd5qbz2"
+export const EMAILJS_SERVICE_ID = "service_dlprier"
 
-export const EMAILJS_PUBLIC_KEY = "user_SXwbm2bfwtluGBhARcTaf"
+export const EMAILJS_PUBLIC_KEY = "rAev2vlTg_8rPxXw1"

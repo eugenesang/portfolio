13be20edfadaa652f6555fed9fe3427ae73b87ca
Eugene Kiprop Sang <eugenekipropsangtg@gmail.com>
2024-04-12 15:13:03 +0300

    removed title repetition

--- src/components/Quotes/index.js
+++ src/components/Quotes/index.js
@@ -10,8 +10,7 @@ function Quote() {
         <div className="w-full h-auto bg-dark-200 py-[50px]">
             <Container>
                 <div className="head mx-auto flex flex-col items-center justify-center text-center md:items-center md:justify-start md:flex-row md:px-0">
-                    <h1 data-aos="fade-right" className="text-[35px] font-bold md:mr-[50px]">Favorite Quote</h1>
-                    <p data-aos="fade-left" className="text-[12px] text-white-200 ">My favorite motivational quote.</p>
+                    <h1 data-aos="fade-right" className="text-[35px] font-bold md:mr-[50px]">Quote</h1>
                 </div>
                 <div id="quote-cont" className='w-full h-auto mt-[100px] relative p-2 md:p-0'>
 
